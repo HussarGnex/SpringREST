5a68d399e75108af372ea110bad2f6cc3caf45d0
HussarGnex <35708971+HussarGnex@users.noreply.github.com>
2018-11-07 19:32:02 +0100

    Change of access modifier

--- src/main/java/cloud/marciniak/rest/repositories/EmployeeRepository.java
+++ src/main/java/cloud/marciniak/rest/repositories/EmployeeRepository.java
@@ -3,6 +3,6 @@ package cloud.marciniak.rest.repositories;
 import cloud.marciniak.rest.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
