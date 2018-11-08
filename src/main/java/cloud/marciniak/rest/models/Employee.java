9d0223f78c1dc659b8a5d7c83a141995a85cf7bc
HussarGnex <35708971+HussarGnex@users.noreply.github.com>
2018-11-08 15:50:46 +0100

    Creation of a default constructor.

--- src/main/java/cloud/marciniak/rest/models/Employee.java
+++ src/main/java/cloud/marciniak/rest/models/Employee.java
@@ -14,6 +14,8 @@ public class Employee {
     private String name;
     private String role;
 
+    public Employee() {}
+
     public Employee(String name, String role) {
         this.name = name;
         this.role = role;
